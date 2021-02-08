31569e10fc20bd75863a3a12235d0a9f9ddb7e1e
Radha Patel <radha.rpatel02@gmail.com>
2021-02-08 14:42:34 -0600

    Maintain input value upon selection

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -5,7 +5,7 @@ class Dropdown extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      timezone: null,
+      input: "",
     };
   }
 
@@ -16,17 +16,23 @@ class Dropdown extends Component {
   }
 
   onChange = (e) => {
+      this.setState({ input: e.target.value })
       document.getElementById("Dropdown").style.display = "block";
       this.props.enterInput(e);
   }
 
+  chooseCity = (e) => {
+      this.props.onSelection(e)
+      this.setState({ input: e.target.id })
+  }
+
   render () {
-      const choices = this.props.results ? this.props.results.map((item, i) => <p key={i} className="Dropdown-result" id={item} onClick={this.props.onSelection}>{item}</p>) : <></>
+      const choices = this.props.results ? this.props.results.map((item, i) => <p key={i} className="Dropdown-result" id={item} onClick={this.chooseCity}>{item}</p>) : <></>
     return (
         <>
             <form className="Dropdown-div">
                 <div>
-                    <input placeholder="Nearest City" onChange={this.onChange} />
+                    <input placeholder="Nearest City" onChange={this.onChange} value={this.state.input} />
                     <div id="Dropdown" className="Dropdown-choices">{choices}</div>
                 </div>
                 <input type="submit" className="Dropdown-submit" onClick={this.closeDropdown} />
